7d6bed74315022ddfd826152b307d63c26367b9b
Sam Morrow <info@sammorrowdrums.com>
2014-12-24 03:11:44 +0000

    Add hello world code

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 8080;
 
+app.get('/:name?', function (req, res) {
+    var name = req.params.name || 'World';
+    res.write('<h1>Hello ' + name);
+});
+
+app.listen(port);
+console.log('Listening on port: ' + port);
